d9e8f6b9b9acd1f5d8ca9c7747025a4a101b51bd
Dogson <myszkowski.lukasz01@gmail.com>
2022-07-14 11:34:08 +0200

    initial commit

---
+++ apps/encaps/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'akademia-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'encaps';
+}
